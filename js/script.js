6abe0569611d937fc234cffc025ee97704289f5c
Olga <andriamiharisoaolga@yahoo.fr>
2024-08-01 18:30:08 +0200

    Mise à jour du 01/08/2024

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
 
     /*---------------Recherche de vidéos par date---------------*/
-    $('#dateSearch').on('input', function() {
+    /*$('#dateSearch').on('input', function() {
         const searchDate = $(this).val();
         let found = false;
         $('.video-content').each(function() {
@@ -28,6 +28,39 @@ $(document).ready(function(){
         } else {
             $('#noResultsMessage').hide();
         }
-    });
+    });*/
+
+        // Fonction de recherche par date
+        function setupDateSearch(sectionId) {
+            const searchInput = `#dateSearch${sectionId}`;
+            const gallery = `#videoGallery${sectionId}`;
+            const noResultsMessage = `#noResultsMessage${sectionId}`;
     
+            $(searchInput).on('input', function() {
+                const searchDate = $(this).val();
+                let found = false;
+                $(gallery).find('.video-content').each(function() {
+                    const videoDate = $(this).find('.date').text();
+                    if (videoDate === searchDate) {
+                        $(this).show();
+                        found = true;
+                    } else {
+                        $(this).hide();
+                    }
+                });
+                if (!found) {
+                    $(noResultsMessage).show();
+                } else {
+                    $(noResultsMessage).hide();
+                }
+            });
+        }
+    
+        // Appliquer la fonction de recherche à chaque section
+        setupDateSearch('Journaux');
+        setupDateSearch('Documentaires');
+        setupDateSearch('Emissions');
+        setupDateSearch('Autres');
+   
 });
+
